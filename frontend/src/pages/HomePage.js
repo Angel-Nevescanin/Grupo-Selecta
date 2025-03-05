7b9145c75e72e78ead6e37ae1d2f312cf1976e9c
Angel-Nevescanin <angel.nevescanin244321@potros.itson.edu.mx>
2025-03-04 19:42:00 -0700

    Update HomePage.js
    Componente de la pagina para mostrar información principal de la aplicación.

---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+    return(
+        <div>
+            <h1>Bievenido a la pagina de inicio</h1>
+            <p>Bievenido al sistema de gestion de productos</p>
+        </div>
+    );
+};
+
+export default HomePage;
