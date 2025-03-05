64b81019e7591875c5ccbb0ddf9a1063fdbb83df
Angel-Nevescanin <angel.nevescanin244321@potros.itson.edu.mx>
2025-03-04 18:08:55 -0700

    Frontend
    Configuración y herramientas para el frontend

--- frontend/js/websocket.js
+++
@@ -1,6 +0,0 @@
-const socket = new WebSocket("ws://localhost:8080");
-
-socket.onmessage = (event) => {
-    console.log("Mensaje recibido:", event.data);
-    alert("Notificación: " + event.data);
-};
